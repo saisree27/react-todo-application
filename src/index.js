bd5164f52c1635f810149afc5d4e2cc7f7683abc
saisree27 <saigautambonam@gmail.com>
2020-06-14 13:07:27 -0400

    Add todo application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './ParticlesComponent.css';
 import App from './App';
+import ParticlesComponent from './ParticlesComponent'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ParticlesComponent/>
+    <App/>
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
